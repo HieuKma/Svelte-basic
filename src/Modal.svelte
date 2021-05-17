231b4c716af3edba29507df178db632bb00000c5
hieuitkma <hieuitkma@gmail.com>
2021-05-17 22:45:56 +0700

    add: conditional style

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,15 +1,39 @@
 <script>
     let showModal = true;
+    let isPromo = true;
+
+    const handleClick = () => {
+        showModal = !showModal;
+    }
 </script>
 
 {#if showModal}
-     <div class="backdrop">
+     <div class="backdrop" class:promo={isPromo}>
          <div class="modal">
-             <p>Sign up for offers</p>
+             <p on:click={handleClick}>Sign up for offers</p>
          </div>
      </div>
 {/if}
 
 <style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background-color: rgba(0, 0, 0, 0.8);
+    }
 
+    .modal {
+        padding: 1px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background-color: #fff;
+    }
+
+    .promo .modal {
+        background-color: crimson;
+        color: white;
+    }
 </style>
