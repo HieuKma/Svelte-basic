33bce9783958299040d88339a85c5a8b8c9f93ca
hieuitkma <hieuitkma@gmail.com>
2021-05-17 17:42:08 +0700

    add: first commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let name;
+	let ten = 'abc';
+	let color = 'black';
+
+	const handleChangeColor = () => {
+		color = 'white'
+	}
+</script>
+
+<main>
+	<h1>Ten tao la:  {ten}</h1>
+	<h1>Hello {name}!</h1>
+	<p>Màu hiện tại là: { color }</p>
+	<button on:click={handleChangeColor}>Change color</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
