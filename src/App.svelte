6643a4940e46f2a867db654cf0f6a20d4a4c1afc
hieuitkma <hieuitkma@gmail.com>
2021-05-17 21:56:34 +0700

    add: Conditionals

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,31 @@
 	let people = [
 		{ id: 1, name: 'Hieu', color: 'red', age: 22 },
 		{ id: 2, name: 'dd', color: 'blue', age: 19 },
-		{ id: 3, name: 'a', color: 'oorange', age: 5 },
+		{ id: 3, name: 'a', color: 'orange', age: 5 },
 	]
 
 	let handleClick = (id) => {
 		people = people.filter(item => item.id !== id)
 	}
+
+	let number = 5;
 </script>
+
+{#if number > 20}
+	<p>Lon hon 20</p>
+{:else if number > 5}
+	<p>Lon hon 5</p>
+{:else}
+	<p>Nho hon hoac bang 5</p>
+{/if}
 
 <main>
 	{#each people as person (person.id)}
 		<h4>{person.name}</h4>
-		<p>{person.color}</p>
+		{#if person.color === 'red' }
+			<p><strong>{person.color}</strong></p>
+		{/if}
+		<p>{person.age} year old, {person.color} belt</p>
 		<button on:click={handleClick(person.id)}>Delete</button>
 	{:else}
 		<p>There are no people to show...</p>
