20df933bba04704a1d5b251d336a9f46aa152716
hieuitkma <hieuitkma@gmail.com>
2021-05-20 21:39:34 +0700

    add: validate form

--- src/components/CreatePoll-form.svelte
+++ src/components/CreatePoll-form.svelte
@@ -1,29 +1,59 @@
 <script>
     import Button from "../shared/Button.svelte";
 
-    let field = {
-        question: '',
-        answerA: '',
-        answerB: ''
-    }
+    let fields = { question: '', answerA: '', answerB: '' };
+    let errors = { question: '', answerA: '', answerB: '' };
+    let valid = false;
 
-    const submitHandeler = () => {
-        console.log(field)
+    const submitHandler = () => {
+        valid = true;
+
+        // validate question 
+        if(fields.question.trim().length < 5) {
+            valid = false;
+            errors.question = 'Question must be at least 5 characters long';
+        } else {
+            errors.question = '';
+        }
+
+        // validate answer A 
+        if(fields.answerA.trim().length < 1) {
+            valid = false;
+            errors.answerA = 'Answer A cannot be empty';
+        } else {
+            errors.answerA = '';
+        }
+
+        // validate answer B
+        if(fields.answerB.trim().length < 1) {
+            valid = false;
+            errors.answerB = 'Answer B cannot be empty';
+        } else {
+            errors.answerB = '';
+        }
+
+        // Add new poll 
+        if (valid) {
+            console.log('valid', fields);
+        }
     }
 </script>
 
-<form on:submit|preventDefault={submitHandeler}>
+<form on:submit|preventDefault={submitHandler}>
     <div class="form-field">
         <label for="question">Poll Question: </label>
-        <input type="text" id="question" bind:value={field.question}>
+        <input type="text" id="question" bind:value={fields.question}>
+        <div class="error">{ errors.question }</div>
     </div>
     <div class="form-field">
         <label for="answer-a">Answer A: </label>
-        <input type="text" id="answer-a" bind:value={field.answerA}>
+        <input type="text" id="answer-a" bind:value={fields.answerA}>
+        <div class="error">{ errors.answerA }</div>
     </div>
     <div class="form-field">
         <label for="answer-b">Answer B: </label>
-        <input type="text" id="answer-b" bind:value={field.answerB}>
+        <input type="text" id="answer-b" bind:value={fields.answerB}>
+        <div class="error">{ errors.answerB }</div>
     </div>
     <Button type='secondary' flat={true} inverse={true}>Add Poll</Button>
 </form>
@@ -34,7 +64,7 @@
         margin: 0 auto;
         text-align: center;
     }
-    .form-field {
+    .form-fields {
         margin: 18px auto;
     }
     input {
@@ -44,5 +74,10 @@
     label {
         margin: 10px auto;
         text-align: left;
+    }
+    .error {
+        font-weight: bold;
+        font-size: 12px;
+        color: #d91b42;
     }
 </style>
